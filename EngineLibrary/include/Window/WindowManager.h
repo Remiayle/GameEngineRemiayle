35da79ec1b4281ded2de6d776ce08fc3927dd5c8
Remiayle <remiayle@gmail.com>
2026-03-16 16:08:36 +0900

    WindowManagerを実装 ウィンドウの移動、サイズ変更、フルスクリーン切り替えに対応 現状はマルチウィンドウ不可で設計

---
+++ EngineLibrary/include/Window/WindowManager.h
@@ -0,0 +1,125 @@
+﻿/*!
+ * @file WindowManager.h
+ * @brief アプリケーションウィンドウの生成・管理を行うクラスの宣言
+ */
+
+#pragma once
+#include <Windows.h>
+#include <string>
+
+namespace Remiayle::Window
+{
+    /*!
+     * @struct WindowManagerConfig
+     * @brief ウィンドウ生成時の初期設定を保持する構造体
+     *
+     * タイトル、サイズ、フルスクリーン設定などを指定します。
+     * 将来的な拡張を見据えて位置指定やリサイズ可否も含みます。
+     */
+    struct WindowManagerConfig
+    {
+        std::wstring title = L"EmptyProject";   /**< ウィンドウタイトル */
+        bool isFullScreen = false;              /**< フルスクリーンで開始するか */
+        UINT width = 1280;                      /**< 初期幅 */
+        UINT height = 720;                      /**< 初期高さ */
+        int posX = CW_USEDEFAULT;               /**< 初期X位置 */
+        int posY = CW_USEDEFAULT;               /**< 初期Y位置 */
+        bool resizable = true;                  /**< リサイズ可能か */
+    };
+
+    /*!
+     * @class WindowManager
+     * @brief アプリケーションウィンドウの生成・管理を行うクラス
+     *
+     * Win32 API を用いてウィンドウを生成し、メッセージ処理、サイズ変更、
+     * フルスクリーン切替などの管理を行います。
+     * DirectX などのレンダリングシステムに HWND やサイズ情報を提供します。
+     */
+    class WindowManager
+    {
+    public:
+        /*!
+         * @brief コンストラクタ
+         * @param config 初期設定
+         */
+        WindowManager(const WindowManagerConfig& config);
+
+        /*!
+         * @brief デストラクタ
+         */
+        ~WindowManager();
+
+        /*!
+         * @brief ウィンドウの初期化
+         * @return true : 成功 false : 失敗
+         */
+        bool Setup();
+
+        /*!
+         * @brief ウィンドウの表示
+         * @return true : 成功 false : 失敗
+         */
+        bool Show();
+
+        /*!
+         * @brief メッセージ処理を行い、アプリ継続可否を返す
+         * @return true : 継続 false : 終了
+         */
+        bool ProcessMessages();
+
+        /*!
+         * @brief フルスクリーン切替
+         * @param enable true でフルスクリーン化
+         */
+        void SetFullScreen(bool enable);
+
+        /*!
+         * @brief ウィンドウハンドル取得
+         * @return HWND
+         */
+        HWND GetHWnd() const { return m_HWnd; }
+
+        /*!
+         * @brief 現在の幅取得
+         * @return 幅
+         */
+        UINT GetWidth() const { return m_Width; }
+
+        /*!
+         * @brief 現在の高さ取得
+         * @return 高さ
+         */
+        UINT GetHeight() const { return m_Height; }
+
+        /*!
+         * @brief フルスクリーン状態取得
+         * @return true : フルスクリーン
+         */
+        bool IsFullScreen() const { return m_IsFullScreen; }
+
+    private:
+        /*!
+         * @brief 静的ウィンドウプロシージャ
+         */
+        static LRESULT CALLBACK StaticWindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+
+        /*!
+         * @brief インスタンス用ウィンドウプロシージャ
+         */
+        LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+
+    private:
+        WindowManagerConfig m_Config;   /**< 初期設定 */
+        HINSTANCE m_HInstance;          /**< インスタンスハンドル */
+        HWND m_HWnd;                    /**< ウィンドウハンドル */
+
+        UINT m_Width;                   /**< 現在の幅 */
+        UINT m_Height;                  /**< 現在の高さ */
+        int m_PosX;                     /**< 現在のX位置 */
+        int m_PosY;                     /**< 現在のY位置 */
+
+        bool m_IsRunning;               /**< メインループ継続フラグ */
+        bool m_IsFullScreen;            /**< フルスクリーン状態 */
+        LONG m_WindowedStyle;           /**< フルスクリーン切替用に保存するウィンドウスタイル */
+    };
+}
---
+++ EngineLibrary/src/WindowManager.cpp
@@ -0,0 +1,190 @@
+﻿/*
+ * @file WindowManager.cpp
+ * @brief アプリケーションウィンドウの生成・管理を行うクラスの実装
+ */
+
+#include "Window/WindowManager.h"
+
+namespace Remiayle::Window
+{
+    /*!
+     * @brief コンストラクタ
+     */
+    WindowManager::WindowManager(const WindowManagerConfig& config)
+        : m_Config(config)
+        , m_HInstance(GetModuleHandle(nullptr))
+        , m_HWnd(nullptr)
+        , m_Width(config.width)
+        , m_Height(config.height)
+        , m_PosX(config.posX)
+        , m_PosY(config.posY)
+        , m_IsRunning(true)
+        , m_IsFullScreen(config.isFullScreen)
+        , m_WindowedStyle(0)
+    {
+    }
+
+    /*!
+     * @brief デストラクタ
+     */
+    WindowManager::~WindowManager()
+    {
+        if (m_HWnd)
+        {
+            DestroyWindow(m_HWnd);
+            m_HWnd = nullptr;
+        }
+    }
+
+    /*!
+     * @brief ウィンドウ初期化
+     */
+    bool WindowManager::Setup()
+    {
+        WNDCLASSEX wc = {};
+        wc.cbSize = sizeof(WNDCLASSEX);
+        wc.lpfnWndProc = StaticWindowProcedure;
+        wc.hInstance = m_HInstance;
+        wc.lpszClassName = m_Config.title.c_str();
+
+        if (!RegisterClassEx(&wc))
+            return false;
+
+        DWORD style = WS_OVERLAPPEDWINDOW;
+        if (!m_Config.resizable)
+            style &= ~(WS_THICKFRAME | WS_MAXIMIZEBOX);
+
+        RECT rc = { 0, 0, (LONG)m_Width, (LONG)m_Height };
+        AdjustWindowRect(&rc, style, FALSE);
+
+        m_HWnd = CreateWindowEx(
+            0,
+            m_Config.title.c_str(),
+            m_Config.title.c_str(),
+            style,
+            m_PosX, m_PosY,
+            rc.right - rc.left,
+            rc.bottom - rc.top,
+            nullptr,
+            nullptr,
+            m_HInstance,
+            this
+        );
+
+        if (!m_HWnd)
+            return false;
+
+        return true;
+    }
+
+    /*!
+     * @brief ウィンドウ表示
+     */
+    bool WindowManager::Show()
+    {
+        ShowWindow(m_HWnd, SW_SHOW);
+        return true;
+    }
+
+    /*!
+     * @brief メッセージ処理
+     */
+    bool WindowManager::ProcessMessages()
+    {
+        MSG msg = {};
+        while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+            {
+                m_IsRunning = false;
+                return false;
+            }
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        return m_IsRunning;
+    }
+
+    /*!
+     * @brief フルスクリーン切替
+     */
+    void WindowManager::SetFullScreen(bool enable)
+    {
+        if (enable == m_IsFullScreen)
+            return;
+
+        if (enable)
+        {
+            m_WindowedStyle = GetWindowLong(m_HWnd, GWL_STYLE);
+
+            MONITORINFO mi = { sizeof(mi) };
+            GetMonitorInfo(MonitorFromWindow(m_HWnd, MONITOR_DEFAULTTOPRIMARY), &mi);
+
+            SetWindowLong(m_HWnd, GWL_STYLE, m_WindowedStyle & ~WS_OVERLAPPEDWINDOW);
+            SetWindowPos(m_HWnd, HWND_TOP,
+                mi.rcMonitor.left, mi.rcMonitor.top,
+                mi.rcMonitor.right - mi.rcMonitor.left,
+                mi.rcMonitor.bottom - mi.rcMonitor.top,
+                SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+        }
+        else
+        {
+            SetWindowLong(m_HWnd, GWL_STYLE, m_WindowedStyle);
+            SetWindowPos(m_HWnd, nullptr,
+                m_PosX, m_PosY, m_Width, m_Height,
+                SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+        }
+
+        m_IsFullScreen = enable;
+    }
+
+    /*!
+     * @brief 静的ウィンドウプロシージャ
+     */
+    LRESULT CALLBACK WindowManager::StaticWindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+    {
+        WindowManager* window = nullptr;
+
+        if (msg == WM_NCCREATE)
+        {
+            CREATESTRUCT* cs = reinterpret_cast<CREATESTRUCT*>(lp);
+            window = reinterpret_cast<WindowManager*>(cs->lpCreateParams);
+            SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)window);
+        }
+        else
+        {
+            window = reinterpret_cast<WindowManager*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+        }
+
+        if (window)
+            return window->WindowProcedure(hwnd, msg, wp, lp);
+
+        return DefWindowProc(hwnd, msg, wp, lp);
+    }
+
+    /*!
+     * @brief インスタンス用ウィンドウプロシージャ
+     */
+    LRESULT WindowManager::WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+    {
+        switch (msg)
+        {
+        case WM_SIZE:
+            m_Width = LOWORD(lp);
+            m_Height = HIWORD(lp);
+            return 0;
+
+        case WM_MOVE:
+            m_PosX = (int)(short)LOWORD(lp);
+            m_PosY = (int)(short)HIWORD(lp);
+            return 0;
+
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            return 0;
+        }
+
+        return DefWindowProc(hwnd, msg, wp, lp);
+    }
+}
